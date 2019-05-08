a8fc4842172a8d41ff2b9a90964ddb518c2551c1
Theodora dimitrova <teddybear90@abv.bg>
2019-05-08 18:17:36 +0300

    change

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Contacts from "./components/Contacts";
 
-import Provider from "./context";
+//import Provider from "./context.js";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Provider>
+    // <Provider>
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
           <Contacts />
         </div>
       </div>
-    </Provider>
+    // </Provider>
   );
 }
 
