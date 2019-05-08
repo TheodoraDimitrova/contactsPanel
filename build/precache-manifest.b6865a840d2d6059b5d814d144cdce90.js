ba32b5ec6c31f6b1076389af52bd9539dc225be3
Theodora dimitrova <teddybear90@abv.bg>
2019-05-08 17:33:08 +0300

    initial commit

---
+++ build/precache-manifest.b6865a840d2d6059b5d814d144cdce90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50ddb7e10d95157ecdced7c79e64368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "771aecf99f6a6b866d9b",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b9d3c3c7ea4ce9047abd",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "771aecf99f6a6b866d9b",
+    "url": "/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "b9d3c3c7ea4ce9047abd",
+    "url": "/static/js/main.9195b42c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
