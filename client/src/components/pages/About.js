a0623f1dcb615d8665800c841047d07ef53c4944
TheodoraDimitrova <teddybear901@abv.bg>
2019-05-31 15:04:19 +0300

    update

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -6,7 +6,7 @@ import React from 'react'
         <div>
             <h1 className="display-4">About Contact book</h1>
             <p className="lead">Simple app to manage contacts</p>
-            
+            <p className="lead">Version 1.0</p>
         </div>
     )
 }
